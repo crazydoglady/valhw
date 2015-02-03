704dc7f7d6b8348f729946541c41513068b75415
CrazyDogLady <valkharvey@gmail.com>
2015-02-03 01:07:36 -0500

    ajax assignment using reddit data

--- ajax/main.js
+++ ajax/main.js
@@ -1,6 +1,10 @@
+currentIndex = 0;
+
+
 
 $(document).ready(function(){
   happy.init();
+
 
 });
 
@@ -13,17 +17,31 @@ var happy = {
   initStyling: function () {
     console.log('init styling');
   },
-  config: {
+  config1: {
     // had to add &jsonp=data to return jsonp instead of json
-    baseUrl: "http://www.reddit.com/r/aww/.json?limit=10&jsonp=data",
+    awwBaseUrl: "http://www.reddit.com/r/aww/.json?limit=100&jsonp=data",
+  },
+  config2: {
+    baBaseUrl: "http://www.reddit.com/r/babyanimals/.json?limit=100&jsonp=data",
+  },
+  config3: {
+    vacationBaseUrl:"http://www.reddit.com/r/EarthPorn/.json?limit=100&jsonp=data",
+  },
+  config4:{
+    foodBaseUrl: "http://www.reddit.com/r/foodPorn/.json?limit=100&jsonp=data",
   },
   initEvents: function () {
-    $('.cute').on('click', happy.getAww());
+    $('#cuteButton').on('click', happy.getAww);
+    $('#animalsButton').on('click', happy.getAnimal);
+    $('#vacationButton').on('click', happy.getVacation);
+    $('#foodButton').on('click', happy.getFood);
+    $('#randomButton').on('click', happy.getRandom);
+
   },
+
   getAww: function () {
-    event.preventDefault();
     $.ajax ({
-      url: happy.config.baseUrl,
+      url: happy.config1.awwBaseUrl,
       type: 'GET',
       dataType: 'JSONP',
       // had to add the next line because the function returned is "callback".  Rename to jsonp.
@@ -32,17 +50,93 @@ var happy = {
       success: function (jsonpCallback) {
         console.log("inside data function");
         console.log(jsonpCallback);
-        var awwws = jsonpCallback.data.children;
-        var markup= "";
-        console.log(awwws);
-        awwws.forEach(function (item, idx, arr) {
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
           console.log (item);
-          $('.cute').closest('section').append('<img src="' +item.data.thumbnail + '">');
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
         });
+        currentIndex ++;
       },
       error: function (error) {
         console.log('error');
       }
-    })
+    });
+  },
+  getAnimal: function () {
+    $.ajax ({
+      url: happy.config2.baBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
+  },
+  getFood: function () {
+    $.ajax ({
+      url: happy.config4.foodBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      // had to add the next line because the function returned is "callback".  Rename to jsonp.
+      // http://stackoverflow.com/questions/20075292/getting-json-from-the-reddit-search-api
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
+  },
+  getVacation: function () {
+    $.ajax ({
+      url: happy.config3.vacationBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
   },
 };
