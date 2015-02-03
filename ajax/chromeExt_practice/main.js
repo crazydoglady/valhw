2e1baff8e689af7f663c5a2b0400a35fc7e1e2e5
CrazyDogLady <valkharvey@gmail.com>
2015-02-03 18:35:24 -0500

    push for chrome extension project. Bad Day band aid is complete for now.

---
+++ ajax/chromeExt_practice/main.js
@@ -0,0 +1,135 @@
+currentIndex = 0;
+
+
+var happy = {
+  init: function () {
+    happy.initStyling();
+    happy.initEvents();
+  },
+
+  initStyling: function () {
+    console.log('init styling');
+  },
+  config1: {
+    // had to add &jsonp=data to return jsonp instead of json
+    awwBaseUrl: "https://www.reddit.com/r/aww/.json?limit=100&jsonp=data",
+  },
+  config2: {
+    baBaseUrl: "https://www.reddit.com/r/babyanimals/.json?limit=100&jsonp=data",
+  },
+  config3: {
+    vacationBaseUrl:"https://www.reddit.com/r/EarthPorn/.json?limit=100&jsonp=data",
+  },
+  config4:{
+    foodBaseUrl: "https://www.reddit.com/r/foodPorn/.json?limit=100&jsonp=data",
+  },
+  initEvents: function () {
+    $('#cuteButton').on('click', happy.getAww);
+    $('#animalsButton').on('click', happy.getAnimal);
+    $('#vacationButton').on('click', happy.getVacation);
+    $('#foodButton').on('click', happy.getFood);
+
+  },
+
+  getAww: function () {
+    $.ajax ({
+      url: happy.config1.awwBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      // had to add the next line because the function returned is "callback".  Rename to jsonp.
+      // http://stackoverflow.com/questions/20075292/getting-json-from-the-reddit-search-api
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
+  },
+  getAnimal: function () {
+    $.ajax ({
+      url: happy.config2.baBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
+  },
+  getFood: function () {
+    $.ajax ({
+      url: happy.config4.foodBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      // had to add the next line because the function returned is "callback".  Rename to jsonp.
+      // http://stackoverflow.com/questions/20075292/getting-json-from-the-reddit-search-api
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
+  },
+  getVacation: function () {
+    $.ajax ({
+      url: happy.config3.vacationBaseUrl,
+      type: 'GET',
+      dataType: 'JSONP',
+      jsonp: "jsonp",
+      success: function (jsonpCallback) {
+        console.log("inside data function");
+        console.log(jsonpCallback);
+        var reddit = jsonpCallback.data.children;
+        console.log(reddit);
+        reddit.forEach(function (item, i, arr) {
+          console.log (item);
+            if (i === currentIndex) {
+            $('.display').prepend('<img src="' + item.data.thumbnail + '">');
+          }
+        });
+        currentIndex ++;
+      },
+      error: function (error) {
+        console.log('error');
+      }
+    });
+  },
+};
+happy.init();
---
+++ mySite/main.js
@@ -0,0 +1,29 @@
+var mySite = {
+  init: funciton() {
+    mysite.initEvents();
+    mySite.initStyling();
+  },
+  initEvents: function() {
+
+  },
+  initStyling: function(){
+
+  },
+}
+config: {
+  url:
+
+}
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  mySite.init();
+
+
+});
