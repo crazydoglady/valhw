e3254a171502943a9aeab3bd5c95645de95f8d4f
CrazyDogLady <valkharvey@gmail.com>
2015-02-05 00:53:18 -0500

    blerg. todo list not working. template error and cannot figure out

---
+++ toDoList/main.js
@@ -0,0 +1,123 @@
+var tasks = {
+  init: function(){
+    tasks.initStyling(); //calls styling functions
+    tasks.initEvents(); //calls Events functions
+
+  },
+  initStyling: function() {
+    tasks.renderTasks(); //adds all to page by calling render function
+
+  },
+  initEvents: function() {
+
+  $('section').on('click', '.showEditTask', function(event){
+    event.preventDefault();
+    $(this).closest('article').find('.editTask').toggleClass('show');
+  }); //end click event for .showEditTask
+
+  $('section').on('submit', '.editTask', function (event){
+    event.preventDefault();
+    var taskid = $(this).closest('article').data('taskid');
+    var editedTask= {
+     name: $(this).find('input[name="editName"]').val(),
+     dueDate: $(this).find('input[name="editDueDate"]').val(),
+     description: ($this).find('textarea[name="editDescription"]').val()
+   }; //end editedTask
+   tasks.updateTask(taskid, editedTask); //calls update function with taskId and new task info
+  }); //end submit for .editTask
+
+  $('#createTask').on('submit', function (event){
+    event.preventDefault();
+    var newTask = {
+      name: $(this).find('input[name="newName"]').val(),
+      dueDate: $(this).find('input[name="newDueDate"]').val(),
+      description: $(this).find('textarea[name="editNewDescription"]').val()
+    };//end newTask
+    tasks.createTask(newTask);
+
+  }); //end submit event for createTask Id
+
+  $('section').on('click', '.deleteTask', function (event) {
+    event.preventDefault();
+    var taskid = $(this).closest('article').data('taskid');
+    console.log(taskid);
+    tasks.deleteTask(taskid);
+  });//end deleteTask click function
+
+},//end events functions
+  config: {
+    url: 'http://tiy-fee-rest.herokuapp.com/collections/valkharvey',
+  },
+  render: function (data, tmpl, $el) {
+    var template = _.template(data, tmpl);
+
+    $el.append(template);
+  },
+  renderTasks: function() {
+    $.ajax({
+      url: tasks.config.url,
+      type:'GET',
+      success: function(tasks) {
+        console.log(tasks);
+        var renderTemp = _.template($('#taskTmpl')).html();
+        var markup = "";
+
+        tasks.forEach(function (item, idx, arr) {
+          console.log(idx)
+          markup += renderTemp(item);
+        });
+        console.log('markup is....', markup);
+        $('section').html(markup);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });//end ajax call for renderTasks
+  },
+  createTask: function(task) {
+    $.ajax({
+      url:tasks.config.url,
+      data: task,
+      type: 'POST', //adds info to server
+      success: function (data) { //if info received from server...
+        console.log(data);
+        tasks.renderTasks(); //calls renderTasks function
+      },
+      error: function (err) {
+        console.log("You f_d up. Do not pass go.")
+      }
+    });//end ajax for create task
+  }, //end create
+  deleteTask: function (id) {
+    $.ajax({
+      url: tasks.config.url + '/' + id,
+      type: 'DELETE', //D is for Delete in CRUD
+      success: function (data) {
+        console.log(data);
+        tasks.renderTasks();
+      },
+      error: function(err) {
+        console.log(err); //You DONE BAD!
+      }
+    }); //end ajax for delete
+  },
+  updateTask: function (id, task) { //shit just got real, yo
+    $.ajax({
+      url: tasks.config.url + '/' + id, //baseUrl adds slash and Id to url. not sure why...
+      data: task,
+      type: 'PUT', //CRUD-update stage
+      success: function (data) {
+        console.log(data);
+        tasks.renderTasks();
+      },
+      error: function (err) {
+        console.log(err); //you f_d up. Try again.
+      }
+    });//end ajax for updateTask
+  }//end update
+}; //end tasks object
+
+$(document).ready(function() {
+  tasks.init();
+
+});
---
+++ toDoList/templates.js
@@ -0,0 +1,23 @@
+var templates = {};
+
+templates.task = [
+"<article data-taskId='<%= _id %>'>",
+"<h3><%= taskName %></h3>",
+"<h4><%= dueDate %></h4>",
+"<p><%= description %></p>",
+"<button class="deleteTask"></button> | <button class="showEditTask"></button>",
+"<form action="" class="editT  ask">",
+"<div class=  "form">",
+"<input type="text" class="control" name="editName" value"<%= name %">",
+"</div>",
+"<div class="form">",
+"<input type="text" class="control" name="editdueDate" value="<% dueDate %>" >",
+"</div>",
+"<div class="form">",
+"<textarea name="editDescription" class="control" id="" cols="30" rows="10  "> <%= description %> </textarea>",
+"</div>",
+"<button class=type="submit"><h2>EditTask</h2></button>",
+"</form>",
+"</article>"
+
+].join("");
